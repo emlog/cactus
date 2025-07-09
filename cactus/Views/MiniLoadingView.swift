1aa560d0c96096623fbc5284445a881045b3ba42
snow <4344235+emlog@users.noreply.github.com>
2025-07-09 09:06:11 +0800

    🔥 自定义loading动画样式

--- cactus/Views/MiniLoadingView.swift
+++ cactus/Views/MiniLoadingView.swift
@@ -8,7 +8,7 @@ struct MiniLoadingView: View {
             .trim(from: 0, to: 0.7)
             .stroke(
                 AngularGradient(
-                    gradient: Gradient(colors: [.accentColor.opacity(0.3), .accentColor]),
+                    gradient: Gradient(colors: [.primary.opacity(0.2), .primary]),
                     center: .center
                 ),
                 style: StrokeStyle(lineWidth: 2, lineCap: .round)
