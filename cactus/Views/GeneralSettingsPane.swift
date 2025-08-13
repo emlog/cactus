a1aafb2d5212c99705c551494d9f725f59c60046
snow <4344235+emlog@users.noreply.github.com>
2025-08-13 23:20:37 +0800

    refactor(GeneralSettingsPane): reorder settings sections for better organization

--- cactus/Views/GeneralSettingsPane.swift
+++ cactus/Views/GeneralSettingsPane.swift
@@ -10,6 +10,85 @@ struct GeneralSettingsPane: View {
         ScrollView {
             VStack(spacing: 0) {
                 VStack{
+                    // 开机自启动
+                    SettingRow(
+                        label: NSLocalizedString("startup", comment: "开机自启动")
+                    ) {
+                        LaunchAtLogin.Toggle{}
+                    }
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    
+                    Divider()
+                        .padding(.horizontal, 10)
+                    
+                    // 默认主窗口功能
+                    SettingRow(
+                        label: NSLocalizedString("default_main_function", comment: "默认主窗口功能"),
+                        description: NSLocalizedString("default_main_function_description", comment: "按下回车键时主窗口默认触发的功能")
+                    ) {
+                        // 在默认主窗口功能的Picker中
+                        Picker("", selection: $preferences.defaultMainFunction) {
+                            ForEach(preferences.functionKeys, id: \.self) { functionKey in
+                                Text(preferences.availableFunctions[functionKey] ?? functionKey).tag(functionKey)
+                            }
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(width: 200, alignment: .leading)
+                    }
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                }
+                .background(Color(NSColor.gridColor))
+                .cornerRadius(12)
+                .padding(16)
+                
+                VStack{
+                    // 常用语言
+                    SettingRow(
+                        label: NSLocalizedString("preferred_language", comment: "常用语言"),
+                        description: NSLocalizedString("preferred_language_description", comment: "翻译的目标语言，一般为母语")
+                    ) {
+                        Picker("", selection: $preferences.preferredLanguage) {
+                            ForEach(preferences.languageKeys, id: \.self) { key in
+                                Text(preferences.availableLanguages[key] ?? key)
+                                    .tag(key)
+                            }
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(width: 200, alignment: .leading)
+                    }
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    
+                    Divider()
+                        .padding(.horizontal, 10)
+                    
+                    // 常用外语
+                    SettingRow(
+                        label: NSLocalizedString("common_foreign_language", comment: "常用外语"),
+                        description: NSLocalizedString("common_foreign_language_description", comment: "正在学习和使用的第一外语")
+                    ) {
+                        Picker("", selection: $preferences.commonForeignLanguage) {
+                            ForEach(preferences.languageKeys, id: \.self) { key in
+                                Text(preferences.availableLanguages[key] ?? key)
+                                    .tag(key)
+                            }
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                        .frame(width: 200, alignment: .leading)
+                    }
+                    .padding(.horizontal, 10)
+                    .padding(.vertical, 5)
+                    
+                    Divider()
+                        .padding(.horizontal, 10)
+                }
+                .background(Color(NSColor.gridColor))
+                .cornerRadius(12)
+                .padding(16)
+
+                VStack{
                     // 打开主窗口快捷键
                     SettingRow(
                         label: NSLocalizedString("shortcut_openmain", comment: "打开主窗口快捷键"),
@@ -97,85 +176,6 @@ struct GeneralSettingsPane: View {
                     }
                     .padding(.horizontal, 10)
                     .padding(.vertical, 5)
-                }
-                .background(Color(NSColor.gridColor))
-                .cornerRadius(12)
-                .padding(16)
-                
-                VStack{
-                    // 开机自启动
-                    SettingRow(
-                        label: NSLocalizedString("startup", comment: "开机自启动")
-                    ) {
-                        LaunchAtLogin.Toggle{}
-                    }
-                    .padding(.horizontal, 10)
-                    .padding(.vertical, 5)
-                    
-                    Divider()
-                        .padding(.horizontal, 10)
-                    
-                    // 默认主窗口功能
-                    SettingRow(
-                        label: NSLocalizedString("default_main_function", comment: "默认主窗口功能"),
-                        description: NSLocalizedString("default_main_function_description", comment: "按下回车键时主窗口默认触发的功能")
-                    ) {
-                        // 在默认主窗口功能的Picker中
-                        Picker("", selection: $preferences.defaultMainFunction) {
-                            ForEach(preferences.functionKeys, id: \.self) { functionKey in
-                                Text(preferences.availableFunctions[functionKey] ?? functionKey).tag(functionKey)
-                            }
-                        }
-                        .pickerStyle(MenuPickerStyle())
-                        .frame(width: 200, alignment: .leading)
-                    }
-                    .padding(.horizontal, 10)
-                    .padding(.vertical, 5)
-                }
-                .background(Color(NSColor.gridColor))
-                .cornerRadius(12)
-                .padding(16)
-                
-                VStack{
-                    // 常用语言
-                    SettingRow(
-                        label: NSLocalizedString("preferred_language", comment: "常用语言"),
-                        description: NSLocalizedString("preferred_language_description", comment: "翻译的目标语言，一般为母语")
-                    ) {
-                        Picker("", selection: $preferences.preferredLanguage) {
-                            ForEach(preferences.languageKeys, id: \.self) { key in
-                                Text(preferences.availableLanguages[key] ?? key)
-                                    .tag(key)
-                            }
-                        }
-                        .pickerStyle(MenuPickerStyle())
-                        .frame(width: 200, alignment: .leading)
-                    }
-                    .padding(.horizontal, 10)
-                    .padding(.vertical, 5)
-                    
-                    Divider()
-                        .padding(.horizontal, 10)
-                    
-                    // 常用外语
-                    SettingRow(
-                        label: NSLocalizedString("common_foreign_language", comment: "常用外语"),
-                        description: NSLocalizedString("common_foreign_language_description", comment: "正在学习和使用的第一外语")
-                    ) {
-                        Picker("", selection: $preferences.commonForeignLanguage) {
-                            ForEach(preferences.languageKeys, id: \.self) { key in
-                                Text(preferences.availableLanguages[key] ?? key)
-                                    .tag(key)
-                            }
-                        }
-                        .pickerStyle(MenuPickerStyle())
-                        .frame(width: 200, alignment: .leading)
-                    }
-                    .padding(.horizontal, 10)
-                    .padding(.vertical, 5)
-                    
-                    Divider()
-                        .padding(.horizontal, 10)
                 }
                 .background(Color(NSColor.gridColor))
                 .cornerRadius(12)
